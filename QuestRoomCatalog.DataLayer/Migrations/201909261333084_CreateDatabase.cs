541c865e7a850fd21736415327444ec5de29faf8
seda8888-92 <seda8888-92@mail.ru>
2019-12-04 14:55:48 +0600

    fixedMigrations

--- QuestRoomCatalog.DataLayer/Migrations/201909261333084_CreateDatabase.cs
+++ QuestRoomCatalog.DataLayer/Migrations/201909261333084_CreateDatabase.cs
@@ -7,144 +7,144 @@ namespace QuestRoomCatalog.DataLayer.Migrations
     {
         public override void Up()
         {
-            //CreateTable(
-            //    "dbo.AspNetRoles",
-            //    c => new
-            //    {
-            //        Id = c.String(nullable: false, maxLength: 128),
-            //        Name = c.String(nullable: false, maxLength: 256),
-            //    })
-            //    .PrimaryKey(t => t.Id);
+            CreateTable(
+                "dbo.AspNetRoles",
+                c => new
+                {
+                    Id = c.String(nullable: false, maxLength: 128),
+                    Name = c.String(nullable: false, maxLength: 256),
+                })
+                .PrimaryKey(t => t.Id);
+
+            CreateTable(
+                "dbo.AspNetUsers",
+                c => new
+                {
+                    Id = c.String(nullable: false, maxLength: 128),
+                    Email = c.String(maxLength: 256),
+                    EmailConfirmed = c.Boolean(nullable: false),
+                    PasswordHash = c.String(),
+                    SecurityStamp = c.String(),
+                    PhoneNumber = c.String(),
+                    PhoneNumberConfirmed = c.Boolean(nullable: false),
+                    TwoFactorEnabled = c.Boolean(nullable: false),
+                    LockoutEndDateUtc = c.DateTime(),
+                    LockoutEnabled = c.Boolean(nullable: false),
+                    AccessFailedCount = c.Int(nullable: false),
+                    UserName = c.String(nullable: false, maxLength: 256),
+                })
+                .PrimaryKey(t => t.Id);
+
+            CreateTable(
+                "dbo.AspNetUserClaims",
+                c => new
+                {
+                    Id = c.Int(nullable: false, identity: true),
+                    UserId = c.String(nullable: false, maxLength: 128),
+                    ClaimType = c.String(),
+                    ClaimValue = c.String(),
+                })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId);
+
+            CreateTable(
+                "dbo.AspNetUserLogins",
+                c => new
+                {
+                    LoginProvider = c.String(nullable: false, maxLength: 128),
+                    ProviderKey = c.String(nullable: false, maxLength: 128),
+                    UserId = c.String(nullable: false, maxLength: 128),
+                })
+                .PrimaryKey(t => new { t.LoginProvider, t.ProviderKey, t.UserId })
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId);
 
             //CreateTable(
-            //    "dbo.AspNetUsers",
+            //    "dbo.__MigrationHistory",
             //    c => new
-            //    {
-            //        Id = c.String(nullable: false, maxLength: 128),
-            //        Email = c.String(maxLength: 256),
-            //        EmailConfirmed = c.Boolean(nullable: false),
-            //        PasswordHash = c.String(),
-            //        SecurityStamp = c.String(),
-            //        PhoneNumber = c.String(),
-            //        PhoneNumberConfirmed = c.Boolean(nullable: false),
-            //        TwoFactorEnabled = c.Boolean(nullable: false),
-            //        LockoutEndDateUtc = c.DateTime(),
-            //        LockoutEnabled = c.Boolean(nullable: false),
-            //        AccessFailedCount = c.Int(nullable: false),
-            //        UserName = c.String(nullable: false, maxLength: 256),
-            //    })
-            //    .PrimaryKey(t => t.Id);
+            //        {
+            //            MigrationId = c.String(nullable: false, maxLength: 150),
+            //            ContextKey = c.String(nullable: false, maxLength: 300),
+            //            Model = c.Binary(nullable: false),
+            //            ProductVersion = c.String(nullable: false, maxLength: 32),
+            //        })
+            //    .PrimaryKey(t => new { t.MigrationId, t.ContextKey });
 
-            //CreateTable(
-            //    "dbo.AspNetUserClaims",
-            //    c => new
-            //    {
-            //        Id = c.Int(nullable: false, identity: true),
-            //        UserId = c.String(nullable: false, maxLength: 128),
-            //        ClaimType = c.String(),
-            //        ClaimValue = c.String(),
-            //    })
-            //    .PrimaryKey(t => t.Id)
-            //    .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
-            //    .Index(t => t.UserId);
+            CreateTable(
+                "dbo.QuestsLogos",
+                c => new
+                {
+                    Id = c.Int(nullable: false, identity: true),
+                    QuestRoomId = c.Int(nullable: false),
+                    Image = c.Binary(nullable: false, maxLength: 1, fixedLength: true),
+                    IsLogo = c.Boolean(nullable: false),
+                })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.QuestsRooms", t => t.QuestRoomId)
+                .Index(t => t.QuestRoomId);
 
-            //CreateTable(
-            //    "dbo.AspNetUserLogins",
-            //    c => new
-            //    {
-            //        LoginProvider = c.String(nullable: false, maxLength: 128),
-            //        ProviderKey = c.String(nullable: false, maxLength: 128),
-            //        UserId = c.String(nullable: false, maxLength: 128),
-            //    })
-            //    .PrimaryKey(t => new { t.LoginProvider, t.ProviderKey, t.UserId })
-            //    .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
-            //    .Index(t => t.UserId);
+            CreateTable(
+                "dbo.QuestsRooms",
+                c => new
+                {
+                    Id = c.Int(nullable: false, identity: true),
+                    Name = c.String(nullable: false, maxLength: 250),
+                    Description = c.String(nullable: false),
+                    TimeForQuest = c.Int(nullable: false),
+                    MaxGamer = c.Int(nullable: false),
+                    MinGamer = c.Int(nullable: false),
+                    MinAgeGamer = c.Int(nullable: false),
+                    FearLevel = c.Int(nullable: false),
+                    ComplexityLevel = c.Int(nullable: false),
+                })
+                .PrimaryKey(t => t.Id);
 
-            ////CreateTable(
-            ////    "dbo.__MigrationHistory",
-            ////    c => new
-            ////        {
-            ////            MigrationId = c.String(nullable: false, maxLength: 150),
-            ////            ContextKey = c.String(nullable: false, maxLength: 300),
-            ////            Model = c.Binary(nullable: false),
-            ////            ProductVersion = c.String(nullable: false, maxLength: 32),
-            ////        })
-            ////    .PrimaryKey(t => new { t.MigrationId, t.ContextKey });
+            CreateTable(
+                "dbo.Rating",
+                c => new
+                {
+                    Id = c.Int(nullable: false, identity: true),
+                    QuestRoomId = c.Int(nullable: false),
+                    RatingLevel = c.Int(nullable: false),
+                })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.QuestsRooms", t => t.QuestRoomId)
+                .Index(t => t.QuestRoomId);
 
-            //CreateTable(
-            //    "dbo.QuestsLogos",
-            //    c => new
-            //    {
-            //        Id = c.Int(nullable: false, identity: true),
-            //        QuestRoomId = c.Int(nullable: false),
-            //        Image = c.Binary(nullable: false, maxLength: 1, fixedLength: true),
-            //        IsLogo = c.Boolean(nullable: false),
-            //    })
-            //    .PrimaryKey(t => t.Id)
-            //    .ForeignKey("dbo.QuestsRooms", t => t.QuestRoomId)
-            //    .Index(t => t.QuestRoomId);
+            CreateTable(
+                "dbo.Roles",
+                c => new
+                {
+                    Id = c.Int(nullable: false, identity: true),
+                    RoleName = c.String(nullable: false, maxLength: 250),
+                })
+                .PrimaryKey(t => t.Id);
 
-            //CreateTable(
-            //    "dbo.QuestsRooms",
-            //    c => new
-            //    {
-            //        Id = c.Int(nullable: false, identity: true),
-            //        Name = c.String(nullable: false, maxLength: 250),
-            //        Description = c.String(nullable: false),
-            //        TimeForQuest = c.Int(nullable: false),
-            //        MaxGamer = c.Int(nullable: false),
-            //        MinGamer = c.Int(nullable: false),
-            //        MinAgeGamer = c.Int(nullable: false),
-            //        FearLevel = c.Int(nullable: false),
-            //        ComplexityLevel = c.Int(nullable: false),
-            //    })
-            //    .PrimaryKey(t => t.Id);
+            CreateTable(
+                "dbo.Users",
+                c => new
+                {
+                    Id = c.Int(nullable: false, identity: true),
+                    UserName = c.String(nullable: false, maxLength: 250),
+                    RoleId = c.Int(nullable: false),
+                })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Roles", t => t.RoleId)
+                .Index(t => t.RoleId);
 
-            //CreateTable(
-            //    "dbo.Rating",
-            //    c => new
-            //    {
-            //        Id = c.Int(nullable: false, identity: true),
-            //        QuestRoomId = c.Int(nullable: false),
-            //        RatingLevel = c.Int(nullable: false),
-            //    })
-            //    .PrimaryKey(t => t.Id)
-            //    .ForeignKey("dbo.QuestsRooms", t => t.QuestRoomId)
-            //    .Index(t => t.QuestRoomId);
-
-            //CreateTable(
-            //    "dbo.Roles",
-            //    c => new
-            //    {
-            //        Id = c.Int(nullable: false, identity: true),
-            //        RoleName = c.String(nullable: false, maxLength: 250),
-            //    })
-            //    .PrimaryKey(t => t.Id);
-
-            //CreateTable(
-            //    "dbo.Users",
-            //    c => new
-            //    {
-            //        Id = c.Int(nullable: false, identity: true),
-            //        UserName = c.String(nullable: false, maxLength: 250),
-            //        RoleId = c.Int(nullable: false),
-            //    })
-            //    .PrimaryKey(t => t.Id)
-            //    .ForeignKey("dbo.Roles", t => t.RoleId)
-            //    .Index(t => t.RoleId);
-
-            //CreateTable(
-            //    "dbo.AspNetUserRoles",
-            //    c => new
-            //    {
-            //        RoleId = c.String(nullable: false, maxLength: 128),
-            //        UserId = c.String(nullable: false, maxLength: 128),
-            //    })
-            //    .PrimaryKey(t => new { t.RoleId, t.UserId })
-            //    .ForeignKey("dbo.AspNetRoles", t => t.RoleId, cascadeDelete: true)
-            //    .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
-            //    .Index(t => t.RoleId)
-            //    .Index(t => t.UserId);
+            CreateTable(
+                "dbo.AspNetUserRoles",
+                c => new
+                {
+                    RoleId = c.String(nullable: false, maxLength: 128),
+                    UserId = c.String(nullable: false, maxLength: 128),
+                })
+                .PrimaryKey(t => new { t.RoleId, t.UserId })
+                .ForeignKey("dbo.AspNetRoles", t => t.RoleId, cascadeDelete: true)
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.RoleId)
+                .Index(t => t.UserId);
 
         }
         
@@ -170,7 +170,7 @@ namespace QuestRoomCatalog.DataLayer.Migrations
             DropTable("dbo.Rating");
             DropTable("dbo.QuestsRooms");
             DropTable("dbo.QuestsLogos");
-            //DropTable("dbo.__MigrationHistory");
+            DropTable("dbo.__MigrationHistory");
             DropTable("dbo.AspNetUserLogins");
             DropTable("dbo.AspNetUserClaims");
             DropTable("dbo.AspNetUsers");
